3cd9ca88ea838faca60e05df0278ebc2f7798cb2
Omer Dolev <odolev@microsoft.com>
2021-08-09 21:29:08 +0300

    draw stones in basic poition

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function () {
     const board = document.getElementById("board");
-    const drawingContext = board.getContext("2d");
+    const ctx = board.getContext("2d");
 
     const intersections = 19;
     const distanceBetweenIntersections = 40;
@@ -18,41 +18,80 @@
         [16, 10],
         [10, 10]
     ];
+    const stones = [
+        ["white", 4, 17],
+        ["white", 3, 16],
+        ["white", 3, 15],
+        ["white", 4, 14],
+        ["white", 4, 13],
+        ["black", 5, 17],
+        ["black", 3, 14],
+        ["black", 3, 13],
+        ["black", 3, 12],
+        ["black", 4, 12],
 
-    requestAnimationFrame(function drawingLoop() {
-        drawingContext.resetTransform();
+        ["white", 17, 3],
+        ["white", 16, 2],
+        ["white", 15, 2],
+        ["white", 15, 3],
+        ["white", 14, 4],
+        ["black", 16, 3],
+        ["black", 14, 3],
+        ["black", 15, 4],
+        ["black", 15, 5],
+        ["black", 17, 5],
+        ["black", 13, 4]
+    ];
 
-        drawingContext.fillStyle = "black";
-        drawingContext.fillRect(0, 0, canvasSize, canvasSize);
+    const cordToPos = n => (n - 1) * distanceBetweenIntersections + boardMargin;
 
-        drawingContext.translate((canvasSize - boardSize) / 2, (canvasSize - boardSize) / 2);
+    requestAnimationFrame(function draw() {
+        ctx.resetTransform();
+
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvasSize, canvasSize);
+
+        ctx.translate((canvasSize - boardSize) / 2, (canvasSize - boardSize) / 2);
 
         drawBoard();
+        drawStones();
 
-        requestAnimationFrame(drawingLoop);
+        requestAnimationFrame(draw);
     });
 
     function drawBoard() {
-        drawingContext.fillStyle = "brown";
-        drawingContext.fillRect(0, 0, boardSize, boardSize);
+        ctx.fillStyle = "brown";
+        ctx.fillRect(0, 0, boardSize, boardSize);
 
-        drawingContext.strokeRect(boardMargin, boardMargin, boardSize - 2 * boardMargin, boardSize - 2 * boardMargin);
+        ctx.strokeRect(boardMargin, boardMargin, boardSize - 2 * boardMargin, boardSize - 2 * boardMargin);
 
         for (let i = 1; i <= intersections - 2; i++) {
-            drawingContext.moveTo(boardMargin, i * distanceBetweenIntersections + boardMargin);
-            drawingContext.lineTo(boardSize - boardMargin, i * distanceBetweenIntersections + boardMargin);
-            drawingContext.moveTo(i * distanceBetweenIntersections + boardMargin, boardMargin);
-            drawingContext.lineTo(i * distanceBetweenIntersections + boardMargin, boardSize - boardMargin);
+            ctx.moveTo(boardMargin, i * distanceBetweenIntersections + boardMargin);
+            ctx.lineTo(boardSize - boardMargin, i * distanceBetweenIntersections + boardMargin);
+            ctx.moveTo(i * distanceBetweenIntersections + boardMargin, boardMargin);
+            ctx.lineTo(i * distanceBetweenIntersections + boardMargin, boardSize - boardMargin);
         }
 
-        drawingContext.stroke();
+        ctx.stroke();
 
-        drawingContext.fillStyle = "black";
+        ctx.fillStyle = "black";
         starPoints.forEach(cords => {
-            const [x, y] = cords.map(n => (n - 1) * distanceBetweenIntersections + boardMargin);
-            drawingContext.beginPath();
-            drawingContext.arc(x, y, 5, 0, 2 * Math.PI, true);
-            drawingContext.fill();
+            const [x, y] = cords.map(cordToPos);
+
+            ctx.beginPath();
+            ctx.arc(x, y, 5, 0, 2 * Math.PI, true);
+            ctx.fill();
         });
+    }
+
+    function drawStones() {
+        stones.forEach(([color, ...cords]) => {
+            const [x, y] = cords.map(cordToPos);
+
+            ctx.beginPath();
+            ctx.arc(x, y, distanceBetweenIntersections / 2, 0, 2 * Math.PI, true);
+            ctx.fillStyle = color;
+            ctx.fill();
+        })
     }
 })();
